555b15fa4d1d5c30674c5d0ed8ef556c5f1aebb2
Febri <achmadfebryanto@gmail.com>
2021-02-21 11:25:04 +0700

    Remove wildcard import

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
     name='moodle-kit',
     packages=['moodle_kit'],
-    version='0.1.1', 
+    version='0.1.3', 
     license='GNU',
     description='A simple Python package with some functionalities to interact with moodle-based LMS.',
     author='Ahmad Febrianto',
     author_email='achmadfebryanto@gmail.com',
     url='https://github.com/ahmadfebrianto/moodle-kit',
-    download_url='https://github.com/ahmadfebrianto/moodle-kit/archive/v0.1.1.tar.gz',
+    download_url='https://github.com/ahmadfebrianto/moodle-kit/archive/v0.1.3.tar.gz',
     keywords=['moodle'],
     install_requires=[
         'requests',
@@ -18,7 +18,6 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
 )
