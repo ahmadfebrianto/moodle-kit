887ed5267ba7580cd204ec04f3d104e839ec2d07
Febri <achmadfebryanto@gmail.com>
2021-02-24 23:13:50 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
 from distutils.core import setup
+from os import path
+
+
+base_dir = path.abspath(path.dirname(__file__))
+with open(path.join(base_dir, 'README.md'), encoding='utf-8') as f:
+    readme = f.read()
 
 setup(
     name='moodle-kit',
     packages=['moodle_kit'],
-    version='0.1.3', 
-    license='GNU',
+    version='0.1.4', 
+    license='GNU General Public License v3.0',
     description='A simple Python package with some functionalities to interact with moodle-based LMS.',
+    long_description=readme,
+    long_description_content_type='text/markdown',
     author='Ahmad Febrianto',
     author_email='achmadfebryanto@gmail.com',
     url='https://github.com/ahmadfebrianto/moodle-kit',
-    download_url='https://github.com/ahmadfebrianto/moodle-kit/archive/v0.1.3.tar.gz',
+    download_url='https://github.com/ahmadfebrianto/moodle-kit/archive/v0.1.4.tar.gz',
     keywords=['moodle'],
     install_requires=[
         'requests',
